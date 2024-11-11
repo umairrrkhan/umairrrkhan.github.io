41b5795d1d4ee3d1f388b22a0561338957736ebe
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2024-11-11 12:27:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class PortfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tech Portfolio',
+      title: 'Umair',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.dark,
